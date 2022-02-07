7fa72d466266caea425e2bcc9257701c84e7bba1
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-02-07 15:08:42 -0300

    configs para OpenID

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc:{
+        clientId: '0oa3towog0qQnX4FH5d7',
+        issuer:'https://dev-66716514.okta.com/oauth2/default',
+        redirectUri:'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
